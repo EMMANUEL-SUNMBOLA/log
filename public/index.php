ea7c6313e6d87417bfb74de82abbd8165e781f7d
Emmanuel Adedayo <adedayoemmanuel729@gmail.com>
2023-02-25 06:03:45 +0100

    8th commit

--- public/index.php
+++ public/index.php
@@ -1,69 +1,7 @@
-<?php 
-        $err = [];
-    if($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST["reg"])){
-        $username = strip_tags($_POST["user"]);
-        $pwd = strip_tags($_POST["pass"]);
-        $pwd2 = strip_tags($_POST["pass2"]);
-        $email = strip_tags($_POST["email"]);
-
-        if(empty($username)){
-            $err[] = "username shouldn't be empty";
-        }elseif(strlen($username) < 5){
-            $err[] = "username should be at least 8 characters long";
-        }
-
-        if(empty($pwd)){
-            $err[] = "please fill password area";
-        }
-        elseif(strlen($pwd) < 8){
-            $err[] = "password too short";
-        }
-        elseif($pwd !== $pwd2){
-            $err[] = "confirm your password";
-        }
-
-        if(empty($email)){
-            $err[] = "email can't be left empty";
-        }
-        // FILTER_VALIDATE_EMAIL
-       elseif(filter_var($email,FILTER_VALIDATE_EMAIL) == false){
-        $err[] = "invalid email <br>";
-       }
-       
-       $file = fopen("../private/text.txt","a+");
-       // loop through to check if the username and / or the email are taken
-       // we won't check for passwordd because more than 1 person can use a password
-       // I think i should check for only gmail 🤔
-           while(!feof($file)){
-               $lin = fgets($file);
-               if($lin == null){
-                $lin = "end|of|file";
-               }
-               $line = explode("|",$lin);
-            //    var_dump($line);
-               if($username == $line[0]){
-                $err[] = "username is taken"; 
-               }
-               if($email == $line[1]){
-                $err[] = "Email has already been used"; 
-               }
-           }
-       if(empty($err)){
-        $file = fopen("../private/text.txt","a+");
-        $pwd2 = password_hash($pwd,PASSWORD_DEFAULT);
-        $message = "\n" . $username . "|" . $email . "|" . $pwd2;
-        fwrite($file,$message);
-        fclose($file);
-        echo '<div class="errs"> youv`e been registered successfully <br> <a href="login.php">Login...</a></div>';
-        // header("locatio n:../private/customer.php");
-       }
-    }
-    else{
-        $err[] = "something went wrong" . "<br>" . "check the following" . "<br>" . "form method" . "<br>" . "button name";
-    }
-?>
-
-<?php require("head.php");?>
+<?php
+ require("../private/reg.php");
+ require("head.php");
+ ?>
 <body>
     <?php
         if(!empty($err) && isset($_POST["reg"])){
--- public/login.php
+++ public/login.php
@@ -1,67 +1,7 @@
 <?php
-$err = [];
-    if(($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["log"])){
-        $userid = strip_tags($_POST["userid"]);
-        $pass = strip_tags($_POST["pass"]);
-
-        if(empty($userid)){
-            $err[] = "please fill username or email";
-        }
-        if(empty($pass)){
-            $err[] = "please fill password";
-        }
-        if(empty($err)){
-            if(filter_var($userid,FILTER_VALIDATE_EMAIL) == TRUE){
-                $file = fopen("../private/text.txt","r");
-                while(!feof($file)){
-                    $lin = fgets($file);
-                    if($lin == null){
-                        $lin = "end|of|file";
-                    }
-                    $line = explode("|",$lin);
-                    if($userid == $line[1]){
-                        if(password_verify($pass,$line[2]) == true){
-                            header("Location:customers/customer.php");
-                        }
-                        else{
-                            $err[] = "wrong  password";
-                        }
-                    }
-                    else{
-                        $err[] = "Inavlid email";
-                    }
-                }
-                fclose($file);
-            }
-            else{
-                $file = fopen("../private/text.txt","r");
-                while(!feof($file)){
-                    $lin = fgets($file);
-                    if($lin == null){
-                        $lin = "end|of|file";
-                    }
-                    $line = explode("|",$lin);
-                    if($userid == $line[0]){
-                       if(password_verify($pass,$line[2]) == true){
-                            header("Location:customers/customer.php");
-                       }
-                       else{
-                        $err[] = "wrong  password";
-                       }
-                    }
-                    else{
-                        $err[] = "Inavlid username";
-                    }
-                }
-                fclose($file);
-            }
-        }
-    }
-    else{
-        $err[] = "something wen't wrong";
-    }
-?>
-<?php require("head.php")?>
+ require("../private/log.php"); 
+ require("head.php");
+ ?>
 <body>
 <?php
         if(!empty($err) && isset($_POST["log"])){
